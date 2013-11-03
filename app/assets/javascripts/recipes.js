9273b38c490adeb8c91334e8b7104516bef06610
Marcus Petzoldt <marcus.petzoldt@gmail.com>
2013-11-03 20:54:46 +0100

    fix qb-9 Add social sharing buttons for g+, fb, twitter

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -118,3 +118,30 @@ function move_tag(tag, from, to) {
         $('div#'+to).append($(tag))
     }
 }
+
+$(document).ready( function like_facebook(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/de_DE/all.js#xfbml=1";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+$(document).ready( function share_twitter(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
+    if(d.getElementById(id)) return;
+    js=d.createElement(s);
+    js.id=id;js.src=p+'://platform.twitter.com/widgets.js';
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'twitter-wjs'));
+
+
+$(document).ready( function() {
+    window.___gcfg = {lang: 'de'};
+});
+
+$(document).ready( function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+}());
