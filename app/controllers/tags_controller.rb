087c4d621ce66d0d5665f8fb5fed6f990e25f127
Marcus Petzoldt <marcus.petzoldt@gmail.com>
2013-11-13 10:08:50 +0100

    fix qb-30

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,7 +5,7 @@ class TagsController < ApplicationController
 
   before_action :require_admin
 
-  PAGINATION = 25
+  PAGINATION = 3
 
   def new
     @tag = Tag.new(category: params[:category])
@@ -20,7 +20,7 @@ class TagsController < ApplicationController
         return
       end
     end
-    redirect_to path_from_category(c)
+    redirect_to path_from_category(c, just_edited: @tag.id)
   end
 
   def edit
@@ -37,7 +37,7 @@ class TagsController < ApplicationController
         return
       end
     end
-    redirect_to path_from_category(c)
+    redirect_to path_from_category(c, just_edited: @tag.id)
   end
 
   def merge
@@ -46,7 +46,7 @@ class TagsController < ApplicationController
       Tagging.where(tag_id: session[:prepare_merge][1..-1]).update_all(tag_id: merge_to)
       Tag.where(id: session[:prepare_merge][1..-1]).destroy_all
       session[:prepare_merge] = nil
-      redirect_to ingredients_path(just_merged: merge_to)
+      redirect_to ingredients_path(just_edited: merge_to)
       return
     end
     redirect_to ingredients_path
@@ -59,12 +59,12 @@ class TagsController < ApplicationController
       session[:prepare_merge] = [] if session[:prepare_merge].nil?
       session[:prepare_merge] << params[:id]
     end
-    redirect_to ingredients_path
+    redirect_to ingredients_path(page: params[:page])
   end
 
   def index
     @category = 0
-    @just_merged = params[:just_merged].to_i if params[:just_merged]
+    @just_edited = params[:just_edited].to_i if params[:just_edited]
     if session[:prepare_merge]
       @tags_to_merge = Tag.find(session[:prepare_merge])
     end
@@ -87,7 +87,12 @@ class TagsController < ApplicationController
     else
       t = Tag.where(category: @category).order(@sort_by => (@sort_direction == 1 ? :asc : :desc))
     end
-    @page = params[:page] ? params[:page].to_i : 0
+    if @just_edited
+      where = t.index(Tag.find(@just_edited)) + 1
+      @page = where / PAGINATION - ((where % PAGINATION > 0) ? 0 : 1)
+    else
+      @page = params[:page] ? params[:page].to_i : 0
+    end
     @max_page = t.count / PAGINATION - ((t.count % PAGINATION > 0) ? 0 : 1)
     @tags = t[@page*PAGINATION..@page*PAGINATION+PAGINATION-1]
   end
@@ -104,7 +109,7 @@ class TagsController < ApplicationController
           flash[:error] = t('views.tags.invalid_tag_has_recipes')
         end
       else
-        flash[:error] = t('view.tags.invalid_tag_id')
+        flash[:error] = t('views.tags.invalid_tag_id')
       end
     end
     redirect_to path_from_category(c)
